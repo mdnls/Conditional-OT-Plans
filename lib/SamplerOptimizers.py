a20b9ae96659d49af49885c77a9c1bd3a165c1c5
Max Daniels <daniels.g@northeastern.edu>
2021-01-16 14:25:38 -0500

    Implement sinkhorn divergence GAN optimizer

--- lib/SamplerOptimizers.py
+++ lib/SamplerOptimizers.py
@@ -2,23 +2,64 @@ import torch
 import os
 
 class SinkSamplerOptimizer():
-    def __init__(self, sampler, compatibiilty, cross_density, negentropy_density, critic_steps):
+    def __init__(self, sampler, target_cpat, gan_training_cpats, critic_steps):
         '''
         Learn the sampler using Sinkhorn Divergence:
 
-        Compute 2OT(a, b) - OT(a, a) - OT(b, b) and minimize with respect to a.
+        Compute 2OT(a, b) - OT(a, a) - OT(b, b) and minimize with respect to a. This requires computing gradients of
+        the first two terms, each of which gets a dedicated compatibility function.
+
+        Arguments:
+            - sampler (ImageSampler): optimize this sampler to be a GAN
+            - target_cpat (Compatibility): the compatibility function of the true OT plan which the generator should learn.
+            - gan_training_cpats (tuple of two Compatibility): two compatibility instances to be trained during the GAN
+                optimization process.
+            - critic_steps (int): number of optimization steps applied to each dual variable of each compatibility function
+                per single GAN step.
         '''
-        pass
+        assert len(gan_training_cpats) == 2, "GAN_training_cpats must be a tuple of two compatibility functions."
+        self.sampler = sampler
+        self.target_cpat = target_cpat
+        self.cross_cpat = gan_training_cpats[0]
+        self.negentropy_cpat = gan_training_cpats[1]
+        self.critic_steps = critic_steps
+
+        # todo: give density parameters a shorter name
+        params = sum([self.cross_cpat.input_density_param_net.parameters(),
+                      self.cross_cpat.output_density_param_net.parameters(),
+                      self.negentropy_cpat.input_density_param_net.parameters(),
+                      self.negentropy_cpat.output_density_param_net.parameters()], [])
+        self.critic_opt = torch.optim.RMSprop(params = params, lr=0.0005)
+        self.sampler_opt = torch.optim.RMSprop(params = self.sampler.params(), lr=0.0005)
+
+    def step(self, inp_sample, outp_sample, z_sample):
+        for _ in range(self.critic_steps):
+            self.critic_opt.step(lambda: self._critic_closure(inp_sample, outp_sample, z_sample))
+        return self.sampler_opt.step(lambda: self._sampler_closure(inp_sample, outp_sample, z_sample))
+
+    def _critic_closure(self, inp_sample, outp_sample, z_sample):
+        # 1. optimize cross OT cost:
+        c_density_real_inp = self.cross_cpat.inp_density_param(inp_sample)
+        c_density_real_outp = self.cross_cpat.outp_density_param(outp_sample)
+        c_density_reg = self.cross_cpat.penalty(inp_sample, outp_sample)
+        c_obj = torch.mean(c_density_real_inp + c_density_real_outp - c_density_reg)
+
+        # 2. optimize negentropy OT cost:
+        ne_density_real_inp = self.negentropy_cpat.inp_density_param(inp_sample)
+        ne_density_real_outp = self.negentropy_cpat.outp_density_param(outp_sample)
+        ne_density_reg = self.negentropy_cpat.penalty(inp_sample, outp_sample)
+        ne_obj = torch.mean(ne_density_real_inp + ne_density_real_outp - ne_density_reg)
+
+        (ne_obj - c_obj).backward() # for gradient ascent rather than descent
 
 class BPSamplerOptimizer():
-    def __init__(self, sampler, compatibility, reg_strength):
+    def __init__(self, sampler, compatibility):
         '''
         Compute the barycentric projection of the learned OT plan.
         '''
         self.sampler = sampler
         self.sampler_opt = torch.optim.RMSprop(sampler.parameters(), lr=0.0005)
         self.compatibility = compatibility
-        self.reg_strength = reg_strength
         self.transport_cost = lambda x, y: torch.mean((x-y)**2, dim=(1, 2, 3))[:, None]
 
     def step(self, inp_batch, outp_batch, z_batch):
